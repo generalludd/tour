0b0e6da5cdd18b6df9f5af5fad1326276a1384d2
Chris Dart <chrisdart@cerebratorium.com>
2013-12-19 21:14:31 -0600

    Clean up navigation bar

--- application/views/page/navigation.php
+++ application/views/page/navigation.php
@@ -1,35 +1,44 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
 
- $buttons[] = array("selection" => "index",
-"text" => "Home",
-"class" => array("button"),
-"href"=> site_url(""),
-"title" => "Home");
- $buttons[] = array("selection" =>"search", "text" => '<input type="text" id="person_search" name="person_search" size="20" value="find people" />', "type" => "pass-through");
+defined('BASEPATH') or exit('No direct script access allowed');
 
-$buttons[] = array("selection" => "tour",
-"text" => "Tours",
-"class" => array("button","show-tours"),
-"href"=> site_url("tour"),
-"title" => "View the list of all tours");
-
-$buttons[] = array("selection" => "person",
-        "text" => "People",
-        "class" => array("button","show-people"),
-        "href"=> site_url("person"),
-        "title" => "View a List of All People");
-/*
-$buttons[] = array("selection" => "all",
-		"text" => "Search Common Names",
-		"class" => array("button","search-common-names"),
-		"type" => "span",
-		"title" => "Search among the common names");
-
-$buttons[] = array("selection" => "color",
-"text" => "Color Listing",
-"class" => array("button"),
-"href" => site_url("color"),
-"title" => "view the color listings",
+$buttons[] = array(
+        "selection" => "index",
+        "text" => "Home",
+        "class" => array(
+                "button"
+        ),
+        "href" => site_url(""),
+        "title" => "Home"
 );
-*/
-print create_button_bar($buttons, array("id" =>"navigation-buttons"));
+$buttons[] = array(
+        "selection" => "search",
+        "text" => '<input type="text" id="person_search" name="person_search" size="20" value="find people" />',
+        "type" => "pass-through"
+);
+
+$buttons[] = array(
+        "selection" => "tour",
+        "text" => "Tours",
+        "class" => array(
+                "button",
+                "show-tours"
+        ),
+        "href" => site_url("tour"),
+        "title" => "View the list of all tours"
+);
+
+$buttons[] = array(
+        "selection" => "person",
+        "text" => "People",
+        "class" => array(
+                "button",
+                "show-people"
+        ),
+        "href" => site_url("person"),
+        "title" => "View a List of All People"
+);
+
+print create_button_bar($buttons, array(
+        "id" => "navigation-buttons"
+));
