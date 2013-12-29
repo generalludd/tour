00477aca85f74e244e76b99281e288162aec4513
Chris Dart <chrisdart@cerebratorium.com>
2013-12-28 19:11:27 -0600

    added delete option for phone listings

--- js/phone.js
+++ js/phone.js
@@ -16,4 +16,22 @@ $(document).ready(function(){
 		});
 		
 	});
+	
+	$(".delete-phone").live("click", function(){
+		my_id = this.id.split("_")[1];
+		form_data= {
+				id: my_id,
+				ajax: "1"
+		};
+		$.ajax({
+			type: "post",
+			url: base_url + "phone/delete",
+			data: form_data,
+			success: function(data){
+				if(data){
+				$("#delete-phone_" + my_id).parents("div.phone-row").remove();
+				}
+			}
+		});
+	});
 });
