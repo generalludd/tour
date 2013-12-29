493f43725dc269b815624d26f0ee1d1f30617b28
Chris Dart <chrisdart@cerebratorium.com>
2013-12-28 18:23:41 -0600

    adjustments for phone lists to work with jQuery in-line editing

--- application/views/phone/view.php
+++ application/views/phone/view.php
@@ -4,10 +4,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 // view.php Chris Dart Dec 13, 2013 6:21:25 PM chrisdart@cerebratorium.com
 
-$output = array();
-foreach ($person->phones as $phone) {
+$output = array(); ?>
+<div id="phone" class="grouping phone-grouping">
+
+<? foreach ($person->phones as $phone) {
     $output[] = sprintf(
-            "<div class='phone-row grouping field-set row' id='phone'>%s<span class='button delete small phone-delete'>Delete</span></div>",
-            create_edit_field("phone",$phone->phone,ucfirst($phone->phone_type),array("envelope"=>"span","id"=>$phone->id)) );
+            "<div class='phone-row field-set row' >%s<span class='button delete small delete-phone' id='delete-phone_%s'>Delete</span></div>",
+            create_edit_field("phone",$phone->phone,ucfirst($phone->phone_type),array("envelope"=>"span","id"=>$phone->id)), $phone->id );
 }
-print implode("\r", $output);
+print implode("\r", $output); ?>
+</div>
