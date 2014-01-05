569ec1d848cd79bf48ebf2dbc7a6cc530e26d72a
Chris Dart <chrisdart@cerebratorium.com>
2014-01-05 17:46:37 -0600

    Troubleshooting the weird logging out mid-use

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -8,6 +8,8 @@ class MY_Controller extends CI_Controller
         parent::__construct();
         if (!is_logged_in($this->session->all_userdata())) {
             // determine the query to redirect after login.
+            $this->load->model("auth_model");
+            $this->auth_model->log(1,"logout");
             $uri = $_SERVER["REQUEST_URI"];
             if ($uri != "/auth") {
                 bake_cookie("uri", $uri);
--- application/helpers/auth_helper.php
+++ application/helpers/auth_helper.php
@@ -2,9 +2,11 @@
 
 function is_logged_in($data)
 {
-	$result = false;
+
 	if(array_key_exists("username", $data) && array_key_exists("role", $data) && array_key_exists("user_id", $data)){
 			$result = true;
+	}else{
+	    $result = false;
 	}
 	return $result;
 
