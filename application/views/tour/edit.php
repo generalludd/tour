a6c018d4d13bd98b51082de90808d52bc1dd2680
Chris Dart <chrisdart@cerebratorium.com>
2014-01-06 12:11:19 -0600

    minor ui adjustments for tour/edit.php

--- application/views/tour/edit.php
+++ application/views/tour/edit.php
@@ -4,49 +4,29 @@
 ?>
 <form name="tour-editor" action="<?=site_url("tour/$action");?>" method="post">
 <input type="hidden" value="<?=get_value($tour, "id");?>" name="id" id="id" />
-<div class="grouping block person-info" id="person">
-<h2><?=get_value($tour, "tour_name");?></h2>
-
-<div class="grouping block tour-info" id="tour">
-<p>
+<div class="block tour-info" id="tour">
 <?=create_input($tour, "tour_name", "Tour Name");?>
-</p>
-<p>
-<?=create_input($tour, "start_date","Start Date", array("format"=>"date", "class"=>"date" ));?>
-</p>
-<p>
-<?=create_input($tour, "end_date", "End Date", array("format"=> "date", "class"=>"date") );?>
-</p>
-<p>
-<?=create_input($tour, "due_date","Due Date", array("format" => "date", "class"=>"date") );?>
-</p>
-<p>
-<?=create_input($tour, "full_price","Full Price $", array("format" => "", "class"=>"money") );?>
-</p>
-<p>
-<?=create_input($tour, "banquet_price","Banquet Price $", array("format" => "", "class"=>"money") );?>
-</p>
-<p>
-<?=create_input($tour, "early_price","Early Price $", array("format" => "", "class"=>"money") );?>
-</p>
-<p>
-<?=create_input($tour, "regular_price","Regular Price $", array("format" => "", "class"=>"money") );?>
-</p>
-<p>
-<?=create_input($tour, "single_room", "Single Room Adjustment $", array("format" => "", "class"=>"money") );?>
-</p>
-<p>
-<?=create_input($tour, "triple_room","Triple Room Adjustment $", array("format" => "", "class"=>"money") );?>
-</p>
-<p>
-<?=create_input($tour, "quad_room","Quad Room Adjustment $", array("format" => "", "class"=>"money") );?>
-</p>
+
+<?=create_input($tour, "start_date","Start Date", array("type"=>"date","format"=>"date", "class"=>"date datefield" ));?>
+
+<?=create_input($tour, "end_date", "End Date", array("type"=>"date","format"=> "date", "class"=>"date datefield") );?>
+
+<?=create_input($tour, "due_date","Due Date", array("type"=>"date","format" => "date", "class"=>"date datefield") );?>
+
+<?=create_input($tour, "full_price","Pay in Ful $", array("type" => "number", "class"=>"money") );?>
+
+<?=create_input($tour, "banquet_price","Banquet Price $", array("type" => "number", "class"=>"money") );?>
+
+<?=create_input($tour, "early_price","Early Price $", array("type" => "number", "class"=>"money") );?>
+<?=create_input($tour, "regular_price","Regular Price $", array("type" => "number", "class"=>"money") );?>
+<?=create_input($tour, "single_room", "Single Room Adjustment $", array("type" => "number", "class"=>"money") );?>
+<?=create_input($tour, "triple_room","Triple Room Adjustment $", array("type" => "number", "class"=>"money") );?>
+<?=create_input($tour, "quad_room","Quad Room Adjustment $", array("type" => "number", "class"=>"money") );?>
 </div>
 <div class='button-box'>
 <ul class='button-list'>
 <li><input type="submit" name="save" id="save" value="<?=$action;?>"/>
 </li>
 </ul>
-</div>
 </div>
 </form>
