4f79e366f797e32599938841d306b9291988b77e
Chris Dart <chrisdart@cerebratorium.com>
2014-01-08 21:49:51 -0600

    Created link to tour in roommate list, refined printing css

--- application/views/roommate/list.php
+++ application/views/roommate/list.php
@@ -5,12 +5,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 $buttons["add_room"] = array("text"=>"Add Room", "class" => "button new add-room", "id"=>sprintf("add-room_%s_%s",$tour_id, $stay), "title"=> "Add a room for this tour and stay");
 $buttons["add_stay"] = array("text"=>"Add Stay (Hotel)", "class" =>"button new add-hotel","id"=>sprintf("add-stay_%s_%s", $tour_id, $stay), "title" => "Add another stay (day) to the tour by adding a hotel");
+
 ?>
-<h3><?=sprintf("Roommates for Tour: %s, Stay: %s",$hotel->tour_name, $stay);?></h3>
+<h3><?=sprintf("Roommates for Tour: <a href='%s'>%s</a>, Stay: %s",site_url("tour/view/$tour_id"), $hotel->tour_name, $stay);?></h3>
 <div class="block hotel-info info-block" id="hotel-info" style="clear:both">
-<label>Hotel:&nbsp;</label><a href="<?=site_url("hotel/view/$hotel->id");?>"><?=$hotel->hotel_name?></a>,&nbsp;
-<label>Arrival Date:&nbsp;</label><?=format_date(get_value($hotel,"arrival_date"));?>,&nbsp;
-<label>Departure Date:&nbsp;</label><?=format_date(get_value($hotel,"departure_date"));?>
+<label>Hotel:&nbsp;</label><a href="<?=site_url("hotel/view/$hotel->id");?>"><?=$hotel->hotel_name?></a>&nbsp;
+<label>Arrival Date:&nbsp;</label><?=format_date(get_value($hotel,"arrival_date"));?>&nbsp;
+<label>Departure Date:&nbsp;</label><?=format_date(get_value($hotel,"departure_date"));?><br/>
+<? if(get_value($hotel, "contact",FALSE)): ?>
+    <label>Contact Info: </label><?=get_value($hotel, "contact");?>,&nbsp;
+<? endif;?>
+<? if(get_value($hotel, "phone",FALSE)): ?>
+<label>Phone: </label><?=get_value($hotel, "phone");?>&nbsp;
+<? endif;?>
+<? if(get_value($hotel, "fax",FALSE)): ?>
+<label>Fax: </label><?=get_value($hotel,"fax");?>&nbsp;
+<? endif;?>
 <?=create_button_bar($buttons);?>
 </div>
 <input type="hidden" id="stay" name="stay" value="<?=$stay;?>"/>
