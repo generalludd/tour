13f7e1895d26f4af69363fdf5d6b8fadfa29436b
Chris Dart <chrisdart@cerebratorium.com>
2014-01-17 21:45:21 -0600

    ability to delete or disable and restore individuals.

--- application/controllers/person.php
+++ application/controllers/person.php
@@ -26,7 +26,6 @@ class Person extends MY_Controller
         // $data["person"] = array();
 
         $person = $this->person->get($id);
-
         $phones = $this->phone->get_for_person($id);
         $person->phones = $phones;
         $person->address = $this->address->get($person->address_id);
@@ -52,15 +51,21 @@ class Person extends MY_Controller
         redirect("person/view/$next_id");
     }
 
-    function view_all ()
+    function view_all ($options = array())
     {
-        $letter = FALSE;
-        if ($this->input->get("letter")) {
-            $letter = $this->input->get("letter");
+        $initial = FALSE;
+        if ($this->input->get("initial")) {
+            $initial = $this->input->get("initial");
+            $options["initial"] = $initial;
         }
+        if ($this->input->get("veterans_only")) {
+            $options["veterans_only"] = TRUE;
+        }
+        // get the list of letters for each of the first initials of last names
+        // in the people table
         $data["initials"] = $this->person->get_initials();
 
-        $data["people"] = $this->person->get_all($letter);
+        $data["people"] = $this->person->get_all($options);
         $data["title"] = "Address Book";
         $data["target"] = "person/list";
         $this->load->view("page/index", $data);
@@ -103,9 +108,21 @@ class Person extends MY_Controller
         $data = array();
         $data["person"] = $this->person->get($id);
         $data["title"] = sprintf("Person Record: %s %s", $data["person"]->first_name, $data["person"]->last_name);
-        // $data["phones"] = $this->phone->get_for_person($id);
+        $this->load->model("variable_model", "variable");
+        $shirt_sizes = $this->variable->get_pairs("shirt_size");
+        $data["shirt_sizes"] = get_keyed_pairs($shirt_sizes, array(
+                "value",
+                "name"
+        ), TRUE);
+        $this->load->model("tourist_model", "tourist");
+        $data["tour_count"] = count($this->tourist->get($id));
         $data["target"] = "person/edit";
-        $this->load->view("page/index", $data);
+        $data["action"] = "update";
+        if ($this->input->get("ajax") == 1) {
+            $this->load->view($data["target"], $data);
+        } else {
+            $this->load->view("page/index", $data);
+        }
     }
 
     function create ()
@@ -113,6 +130,7 @@ class Person extends MY_Controller
         // create a record in the db and get the insertion id. Then go to the
         // edit user page with
         $data["person"] = FALSE;
+        $data["tour_count"] = 0;
         $this->load->model("variable_model", "variable");
         $shirt_sizes = $this->variable->get_pairs("shirt_size");
         $data["shirt_sizes"] = get_keyed_pairs($shirt_sizes, array(
@@ -158,5 +176,28 @@ class Person extends MY_Controller
         );
         $this->person->update($id, $values);
         echo $this->input->post("value");
+    }
+
+    /**
+     * the person_model determines whether to delete a person or just disable
+     * them.
+     * If a person is in the tourist database they will only be disabled.
+     * See person_model->delete for more details.
+     */
+    function delete ()
+    {
+        $id = $this->input->get_post("id");
+        if ($id) {
+            $this->person->delete($id);
+        }
+        redirect("person/view_all");
+    }
+
+    function restore ()
+    {
+        $id = $this->input->post("id");
+        if ($id) {
+            $this->person->restore($id);
+        }
     }
 }
