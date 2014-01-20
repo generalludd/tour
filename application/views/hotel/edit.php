0324749a9f80a9f7a4339098e8d29317adf65c8b
Chris Dart <chrisdart@cerebratorium.com>
2014-01-19 20:12:32 -0600

    Minor finishing touches on contact editing and change to hotel display in hotel/edit view.

--- application/views/hotel/edit.php
+++ application/views/hotel/edit.php
@@ -12,19 +12,22 @@
 <label for="tour_id">Tour:&nbsp;</label>
 <?=form_dropdown("tour_id",$tour_list,get_value($hotel,"tour_id",$tour->id),"id='tour_id'");?>
 </div>
+<div class="input-block=row">
 <?=create_input($hotel, "stay", "Tour Stay Number",$options = array("envelope"=>"div","type"=>"number"));?>
 <?=create_input($hotel, "arrival_date", "Arrival Date", $options = array("envelope"=>"div","envelope_class"=>"inline","format"=>"date","type"=>"date","class"=>"datefield"));?>
-
+</div>
 <?=create_input($hotel, "arrival_time", "Arrival Time", $options = array("envelope"=>"div","format"=>"time","type"=>"time"));?>
-
+<div class="input-block-row">
 <?=create_input($hotel, "departure_date", "Departure Date", $options = array("envelope"=>"div","envelope_class"=>"inline","format"=>"date","type"=>"date","class"=>"datefield"));?>
-
-
 <?=create_input($hotel, "departure_time", "Departure Time", $options = array("envelope"=>"div","format"=>"time","type"=>"time"));?>
-
+</div>
+<div class="input-block=row">
 <?=create_input($hotel, "phone", "Phone", $options = array("envelope"=>"div","envelope_class"=>"inline","format"=>"tel","type"=>"tel"));?>
 <?=create_input($hotel, "fax", "Fax", $options = array("envelope"=>"div","format"=>"tel","type"=>"tel"));?>
-<div class="contacts">
+</div>
+<h5 class="notice">Note: You can add special contacts after you click "<?=ucfirst($action);?>"</h5>
+
+<!--<div class="contacts">
 <fieldset class="triptych column">
 <legend>First Contact</legend>
 <?=create_input($hotel, "first_contact", "Name",array("envelope"=>"div","label_class"=>"above"));?>
@@ -48,9 +51,9 @@
 <?=create_input($hotel, "third_contact_email", "Email",array("envelope"=>"div","label_class"=>"above"));?>
 </fieldset>
 </div>
+-->
 <div class="block">
 <?=create_input($hotel, "address","Address",array("envelope"=>"div","class"=>"address-field"));?>
-
 </div>
 <div class="block">
 <?=create_input($hotel, "note", "Notes", array("envelope"=>"div","class"=>"note-field"));?>
