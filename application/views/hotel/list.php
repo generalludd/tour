e8eecc277dc5f634aeb879c17efd8886e3fc05cc
Chris Dart <chrisdart@cerebratorium.com>
2014-01-19 18:01:52 -0600

    More usability improvements(?) for hotel listings

--- application/views/hotel/list.php
+++ application/views/hotel/list.php
@@ -32,12 +32,13 @@ $buttons[] = array(
 	<thead>
 		<tr>
 			<th>Name</th>
+						<th></th>
+			<th></th>
+
 			<th>Arrival</th>
 			<th>Departure</th>
 			<th>Phone</th>
 			<th>Fax</th>
-			<th></th>
-			<th></th>
 		</tr>
 	</thead>
 	<tbody>
@@ -46,6 +47,13 @@ $buttons[] = array(
 			<td><a
 				class="view-hotel"
 				href="<?=site_url("hotel/view/$hotel->id");?>" title="View hotel details"><?=$hotel->hotel_name;?></a></td>
+				<td><span
+				class="button edit edit-hotel"
+				id="<?=sprintf("edit-hotel_%s",$hotel->id);?>">Edit Hotel</span></td>
+			<td><a
+				href="<?=site_url("roommate/view_for_tour/?tour_id=$tour->id&stay=$hotel->stay");?>"
+				class="button view-roommates"
+				title="Show all roommates for this hotel">Roommates</a></td>
 			<td><?=format_date($hotel->arrival_date);?>,
 			<?=format_time($hotel->arrival_time);?></td>
 			<td>
@@ -54,13 +62,7 @@ $buttons[] = array(
 </td>
 			<td><?=$hotel->phone;?></td>
 			<td><?=$hotel->fax;?></td>
-			<td><span
-				class="button edit edit-hotel"
-				id="<?=sprintf("edit-hotel_%s",$hotel->id);?>">Edit Hotel</span></td>
-			<td><a
-				href="<?=site_url("roommate/view_for_tour/?tour_id=$tour->id&stay=$hotel->stay");?>"
-				class="button view-roommates"
-				title="Show all roommates for this hotel">Roommates</a></td>
+
 		</tr>
 <? endforeach; ?>
 </tbody>
