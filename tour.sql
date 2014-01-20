e843c90d179b44758a72fa3b670918c5748ff697
Chris Dart <chrisdart@cerebratorium.com>
2014-01-19 20:36:44 -0600

    Updates to database schema file.

--- tour.sql
+++ tour.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # http://code.google.com/p/sequel-pro/
 #
-# Host: localhost (MySQL 5.6.15)
+# Host: 127.0.0.1 (MySQL 5.5.31-0ubuntu0.12.04.2)
 # Database: tours
-# Generation Time: 2013-12-29 01:34:03 +0000
+# Generation Time: 2014-01-20 02:33:13 +0000
 # ************************************************************
 
 
@@ -28,12 +28,82 @@ DROP TABLE IF EXISTS `address`;
 CREATE TABLE `address` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `num` varchar(25) DEFAULT NULL,
-  `street` varchar(50) DEFAULT NULL,
-  `unit_type` varchar(10) DEFAULT NULL,
-  `unit` varchar(10) DEFAULT NULL,
+  `street` varchar(65) DEFAULT NULL,
+  `unit` varchar(35) DEFAULT NULL,
   `city` varchar(25) NOT NULL DEFAULT '',
   `state` varchar(2) NOT NULL DEFAULT 'MN',
   `zip` varchar(10) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table contact
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `contact`;
+
+CREATE TABLE `contact` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `hotel_id` int(11) NOT NULL,
+  `contact` varchar(50) NOT NULL DEFAULT '',
+  `position` varchar(55) DEFAULT NULL,
+  `phone` varchar(25) DEFAULT NULL,
+  `fax` varchar(25) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Contacts for hotels and possibly other future businesses that might be included in the tour planning';
+
+
+
+# Dump of table hotel
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `hotel`;
+
+CREATE TABLE `hotel` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `hotel_name` varchar(255) NOT NULL DEFAULT '',
+  `tour_id` int(11) NOT NULL,
+  `stay` int(11) DEFAULT NULL,
+  `arrival_date` date DEFAULT NULL,
+  `arrival_time` varchar(7) DEFAULT NULL,
+  `departure_date` date DEFAULT NULL,
+  `departure_time` varchar(7) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `fax` varchar(20) DEFAULT NULL,
+  `url` varchar(90) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `first_contact` varchar(90) DEFAULT NULL,
+  `first_contact_position` varchar(30) DEFAULT NULL,
+  `first_contact_phone` varchar(25) DEFAULT NULL,
+  `first_contact_email` varchar(50) DEFAULT NULL,
+  `second_contact` varchar(90) DEFAULT NULL,
+  `second_contact_position` varchar(30) DEFAULT NULL,
+  `second_contact_phone` varchar(25) DEFAULT NULL,
+  `second_contact_email` varchar(50) DEFAULT NULL,
+  `third_contact` varchar(90) DEFAULT NULL,
+  `third_contact_position` varchar(30) DEFAULT NULL,
+  `third_contact_phone` varchar(25) DEFAULT NULL,
+  `third_contact_email` varchar(50) DEFAULT NULL,
+  `address` text,
+  `note` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table message
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `message` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `tour_id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL DEFAULT '',
+  `body` text NOT NULL,
+  `message_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -54,6 +124,7 @@ CREATE TABLE `payer` (
   `notes` text,
   `is_comp` tinyint(1) DEFAULT '0',
   `is_cancelled` tinyint(1) DEFAULT '0',
+  `note` text,
   PRIMARY KEY (`payer_id`,`tour_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -72,6 +143,7 @@ CREATE TABLE `person` (
   `email` varchar(60) DEFAULT NULL,
   `shirt_size` varchar(10) DEFAULT NULL,
   `salutation` varchar(100) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -86,6 +158,7 @@ CREATE TABLE `phone` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `phone` varchar(25) NOT NULL DEFAULT '',
   `phone_type` varchar(10) DEFAULT NULL,
+  `person_link` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -102,6 +175,54 @@ CREATE TABLE `phone_person` (
   `person_id` int(11) DEFAULT NULL,
   `is_primary` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table receipt
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `receipt`;
+
+CREATE TABLE `receipt` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `message_id` int(11) NOT NULL,
+  `person_id` int(11) NOT NULL,
+  `status` varchar(25) NOT NULL DEFAULT '',
+  `body` text,
+  `receipt_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `message_id` (`message_id`,`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table room
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `room`;
+
+CREATE TABLE `room` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `tour_id` int(11) DEFAULT NULL,
+  `stay` int(11) DEFAULT NULL,
+  `room` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table roommate
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `roommate`;
+
+CREATE TABLE `roommate` (
+  `person_id` int(11) NOT NULL DEFAULT '0',
+  `tour_id` int(11) NOT NULL DEFAULT '0',
+  `stay` int(11) NOT NULL DEFAULT '0',
+  `room` int(11) NOT NULL,
+  PRIMARY KEY (`person_id`,`tour_id`,`stay`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -152,18 +273,34 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(55) NOT NULL,
-  `password` varchar(32) NOT NULL,
+  `password` varchar(32) DEFAULT '',
   `first_name` varchar(20) NOT NULL,
   `last_name` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL,
   `is_verified` tinyint(1) DEFAULT NULL,
-  `reset_hash` varchar(32) NOT NULL COMMENT 'used for verifying password resets',
+  `reset_hash` varchar(32) DEFAULT '' COMMENT 'used for verifying password resets',
   `is_active` tinyint(1) NOT NULL,
   `role` enum('user','admin') NOT NULL,
-  `start_date` date DEFAULT NULL COMMENT 'Date joined the household',
-  `end_date` date DEFAULT NULL COMMENT 'Date left the household',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table user_log
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_log`;
+
+CREATE TABLE `user_log` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(4) NOT NULL,
+  `username` varchar(55) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `action` enum('login','logout') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `logTime` (`time`),
+  KEY `logAction` (`action`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='database for tracking user logins/logouts';
 
 
 
@@ -197,6 +334,27 @@ CREATE TABLE `variable` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `variable` (`id`, `class`, `name`, `value`, `type`)
+VALUES
+	(13, 'room_size', 'Double', 'double_room', 'varchar'),
+	(14, 'room_size', 'Single', 'single_room', 'varchar'),
+	(15, 'room_size', 'Triple', 'triple_room', 'varchar'),
+	(16, 'room_size', 'Quad', 'quad_room', 'varchar'),
+	(17, 'payment_type', 'Pay in Full', 'full_price', 'varchar'),
+	(18, 'payment_type', 'Banquet Price', 'banquet_price', 'varchar'),
+	(19, 'payment_type', 'Early Bird Price', 'early_price', 'varchar'),
+	(20, 'payment_type', 'Regular Price', 'regular_price', 'varchar'),
+	(22, 'shirt_size', 'S', 'S', 'varchar'),
+	(23, 'shirt_size', 'M', 'M', 'varchar'),
+	(24, 'shirt_size', 'L', 'L', 'varchar'),
+	(25, 'shirt_size', 'XL', 'XL', 'varchar'),
+	(26, 'shirt_size', 'XXL', 'XXL', 'varchar'),
+	(27, 'shirt_size', 'XXXL', 'XXXL', 'varchar'),
+	(28, 'shirt_size', 'XXXXL', 'XXXXL', 'varchar'),
+	(32, 'user_status', 'active', '1', 'varchar'),
+	(33, 'user_status', 'inactive', '0', 'varchar'),
+	(34, 'user_role', 'Administrator', 'admin', 'varchar'),
+	(35, 'user_role', 'User', 'user', 'varchar');
 
 
 
