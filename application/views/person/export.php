e9694d177c13543e83a5337cf0cf106f030a4e19
Chris Dart <chrisdart@cerebratorium.com>
2014-01-28 19:13:45 -0600

    adjustments to improve person/export.php view. Still more work needed.

--- application/views/person/export.php
+++ application/views/person/export.php
@@ -1,22 +1,24 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
 // export.php Chris Dart Jan 18, 2014 3:12:18 PM chrisdart@cerebratorium.com
-
-
-$file_name="people.csv";
-$output = array("First Name,Last Name,email, House Number, Street, Apartment, City, State, Zip");
-foreach($people as $person) {
+$file_name = "people.csv";
+$output = array(
+        "First Name,Last Name,email, Address, City, State, Zip"
+);
+$current_address = $people[0]->address_id;
+$names = array();
+foreach ($people as $person) {
     $line[] = $person->first_name;
     $line[] = $person->last_name;
     $line[] = $person->email;
-    $line[] = $person->num;
-    $line[] = $person->street;
-    $line[] = $person->unit;
+    $line[] = $person->address;
     $line[] = $person->city;
     $line[] = $person->state;
     $line[] = $person->zip;
-    $output[] = implode(",",$line);
+    $output[] = implode(",", $line);
     $line = NULL;
+    $current_address = $person->address_id;
 }
 
 $data = implode("\n", $output);
