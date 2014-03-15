bab6bb7770609ae34d52768b53dbbdaa6e8a43cb
Chris Dart <chrisdart@cerebratorium.com>
2014-03-15 11:49:44 -0500

    moved from beta to full version 1.0

--- application/config/constants.php
+++ application/config/constants.php
@@ -36,6 +36,6 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
-define('APP_VERSION',"1.0-beta-10.0");
+define('APP_VERSION',"1.0");
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
