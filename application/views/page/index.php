baf32407e1f47e3746f08b0c2fa760514cb02587
Chris Dart <chrisdart@cerebratorium.com>
2014-03-15 11:56:36 -0500

    shift from html4 to explicitly html 5

--- application/views/page/index.php
+++ application/views/page/index.php
@@ -9,7 +9,7 @@ if($this->uri->segment(1) == ""){
 	$body_class = "front";
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html>
 <head>
 <? $this->load->view('page/head');?>
