390688c1707b8720f0f68f5f1c6b45eee5127c8e
Chris Dart <chrisdart@cerebratorium.com>
2014-03-15 20:16:15 -0500

    removal of inline editing on tour view page

--- application/views/tour/view.php
+++ application/views/tour/view.php
@@ -14,16 +14,16 @@ $buttons["hotels"] = array("text"=> "Hotels","href"=>site_url("hotel/view_all/$t
 <? print create_button_bar($buttons);
 ?>
 <div class="grouping block tour-info" id="tour">
-<?=create_edit_field("start_date", format_date(get_value($tour, "start_date")),"Start Date", array("envelope" => "p") );?>
-<?=create_edit_field("end_date", format_date(get_value($tour, "end_date")),"End Date", array("envelope" => "p") );?>
-<?=create_edit_field("due_date", format_date(get_value($tour, "due_date")),"Due Date", array("envelope" => "p") );?>
-<?=create_edit_field("full_price", get_value($tour, "full_price"),"Full Price $", array("envelope" => "p") );?>
-<?=create_edit_field("banquet_price", get_value($tour, "banquet_price"),"Banquet Price $", array("envelope" => "p") );?>
-<?=create_edit_field("early_price", get_value($tour, "early_price"),"Early Price $", array("envelope" => "p") );?>
-<?=create_edit_field("regular_price", get_value($tour, "regular_price"),"Regular Price $", array("envelope" => "p") );?>
-<?=create_edit_field("single_room", get_value($tour, "single_room"),"Single Room Adjustment $", array("envelope" => "p") );?>
-<?=create_edit_field("triple_room", get_value($tour, "triple_room"),"Triple Room Adjustment (include a '-') $", array("envelope" => "p") );?>
-<?=create_edit_field("quad_room", get_value($tour, "quad_room"),"Quad Room Adjustment (include a '-')$", array("envelope" => "p") );?>
+<?=create_field("start_date", format_date(get_value($tour, "start_date")),"Start Date", array("envelope" => "p") );?>
+<?=create_field("end_date", format_date(get_value($tour, "end_date")),"End Date", array("envelope" => "p") );?>
+<?=create_field("due_date", format_date(get_value($tour, "due_date")),"Due Date", array("envelope" => "p") );?>
+<?=create_field("full_price", get_value($tour, "full_price"),"Full Price $", array("envelope" => "p") );?>
+<?=create_field("banquet_price", get_value($tour, "banquet_price"),"Banquet Price $", array("envelope" => "p") );?>
+<?=create_field("early_price", get_value($tour, "early_price"),"Early Price $", array("envelope" => "p") );?>
+<?=create_field("regular_price", get_value($tour, "regular_price"),"Regular Price $", array("envelope" => "p") );?>
+<?=create_field("single_room", get_value($tour, "single_room"),"Single Room Adjustment $", array("envelope" => "p") );?>
+<?=create_field("triple_room", get_value($tour, "triple_room"),"Triple Room Adjustment (include a '-') $", array("envelope" => "p") );?>
+<?=create_field("quad_room", get_value($tour, "quad_room"),"Quad Room Adjustment (include a '-')$", array("envelope" => "p") );?>
 </div>
 <div class="letter-list-block" id="tour-letters">
 <? $data["letters"] = $letters;
