6d582acc1de8f17ba50f76bd9c00e60db7c5b858
Chris Dart <chrisdart@cerebratorium.com>
2014-05-31 15:45:14 -0500

    clean up page/head.php view

--- application/views/page/head.php
+++ application/views/page/head.php
@@ -6,39 +6,34 @@
 <!--<meta name="viewport"
 		content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 		--><title><?=$title;?></title>
-<link type="text/css" rel="stylesheet" media="all" href="<?=base_url()?>css/main.css" />
-<link type="text/css" rel="stylesheet" media="screen" href="<?=base_url()?>css/color.css"/>
-<link type="text/css" rel="stylesheet" media="all" href="<?=base_url()?>css/popup.css" />
-<link type="text/css" rel="stylesheet" media="print" href="<?=base_url()?>css/print.css" />
+<link type="text/css" rel="stylesheet" media="all" href="<?=base_url('css/main.css')?>" />
+<link type="text/css" rel="stylesheet" media="screen" href="<?=base_url('css/color.css')?>"/>
+<link type="text/css" rel="stylesheet" media="all" href="<?=base_url('css/popup.css')?>" />
+<link type="text/css" rel="stylesheet" media="print" href="<?=base_url('css/print.css')?>" />
 <!-- jquery scripts -->
 <script type="text/javascript">
 var base_url = '<?=base_url("index.php") . "/";?>';
 var root_url = '<?=base_url();?>';
 </script>
-<!-- <script type="text/javascript" src="<?=base_url();?>js/jquery.min.js"></script>
-<script type="text/javascript" src="<?=base_url();?>js/jquery-ui.min.js"></script> -->
 
 <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.min.js"></script>
 
-<!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script> -->
-<!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script> -->
-
 <!-- Custom Scripts  -->
-<script type="text/javascript" src="<?=base_url();?>js/general.js"></script>
-<script type="text/javascript" src="<?=base_url();?>js/tour.js"></script>
-<script type="text/javascript" src="<?=base_url();?>js/payer.js"></script>
-<script type="text/javascript" src="<?=base_url();?>js/person.js"></script>
-<script type="text/javascript" src="<?=base_url();?>js/address.js"></script>
-<script type="text/javascript" src="<?=base_url();?>js/hotel.js"></script>
-<script type="text/javascript" src="<?=base_url();?>js/contact.js"></script>
+<script type="text/javascript" src="<?=base_url("js/general.js");?>"></script>
+<script type="text/javascript" src="<?=base_url('js/tour.js');?>"></script>
+<script type="text/javascript" src="<?=base_url('js/payer.js');?>"></script>
+<script type="text/javascript" src="<?=base_url('js/person.js');?>"></script>
+<script type="text/javascript" src="<?=base_url('js/address.js');?>"></script>
+<script type="text/javascript" src="<?=base_url('js/hotel.js');?>"></script>
+<script type="text/javascript" src="<?=base_url('js/contact.js');?>"></script>
 
-<script type="text/javascript" src="<?=base_url();?>js/phone.js"></script>
-<script type="text/javascript" src="<?=base_url();?>js/receipt.js"></script>
+<script type="text/javascript" src="<?=base_url('js/phone.js');?>"></script>
+<script type="text/javascript" src="<?=base_url('js/receipt.js');?>"></script>
 
-<script type="text/javascript" src="<?=base_url();?>js/roommate.js"></script>
-<script type="text/javascript" src="<?=base_url();?>js/payment.js"></script>
+<script type="text/javascript" src="<?=base_url('js/roommate.js');?>"></script>
+<script type="text/javascript" src="<?=base_url('js/payment.js');?>"></script>
 
 <!-- admin scripts -->
-<script type="text/javascript" src="<?=base_url();?>js/password.js"></script>
+<script type="text/javascript" src="<?=base_url('js/password.js');?>"></script>
