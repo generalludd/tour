c8bc65d8166abfc68d9db6d42cec9954fafaca2c
chrisdart <chrisdart@sandbox.(none)>
2016-02-02 20:41:37 -0600

    changes

---
+++ application/logs/log-2016-02-02.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2016-02-02 19:43:54 --> Config Class Initialized
+DEBUG - 2016-02-02 19:43:54 --> Hooks Class Initialized
+DEBUG - 2016-02-02 19:43:54 --> Utf8 Class Initialized
+DEBUG - 2016-02-02 19:43:54 --> UTF-8 Support Enabled
+DEBUG - 2016-02-02 19:43:54 --> URI Class Initialized
+DEBUG - 2016-02-02 19:43:54 --> Router Class Initialized
+DEBUG - 2016-02-02 19:43:54 --> Output Class Initialized
+DEBUG - 2016-02-02 19:43:54 --> Security Class Initialized
+DEBUG - 2016-02-02 19:43:54 --> Input Class Initialized
+DEBUG - 2016-02-02 19:43:54 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-02-02 19:43:54 --> Language Class Initialized
