b47bf2481b53817bbbc3592c23183db06ac5882e
Chris Dart <chrisdart@darter.localdomain>
2020-01-14 09:35:15 -0600

    hotfix 3.5.8 -- inserting payments and reimbursement bug

--- application/views/payment/insert.php
+++ application/views/payment/insert.php
@@ -1,13 +1,24 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 // edit.php Chris Dart Mar 9, 2014 7:48:24 PM chrisdart@cerebratorium.com
-$buttons["insert_payment"] = array("text"=>"Insert", "class"=>"button new insert-$type", "id"=>sprintf("insert-%s_%s_%s",$type, $tour_id, $payer_id),"type"=>"span");
+$buttons['insert_payment'] = [
+	'text' => 'Insert',
+	'class' => 'button new insert-' . $type,
+	'href' => base_url('payment/insert/' . $type ),
+	'data' => [
+		'type' => $type,
+		'tour_id' => $tour_id,
+		'payer_id' => $payer_id,
+		'target' => '#' . $type . 's',
+	],
+];
 ?>
-<tr id="<?php print sprintf("insert-row_%s_%s", $tour_id, $payer_id);?>">
-<td><input type="text" class="datefield" name="receipt_date" id="receipt_date"/>
-</td>
-<td><input type="number" name="amount" id="amount" value=""/></td>
-<td>
-<?php print create_button_bar($buttons);?>
-</td>
+<tr id="<?php print sprintf("insert-row_%s_%s", $tour_id, $payer_id); ?>">
+	<td><input type="text" class="datefield" name="receipt_date"
+						 id="receipt_date"/>
+	</td>
+	<td><input type="number" name="amount" id="amount" value=""/></td>
+	<td>
+		<?php print create_button_bar($buttons); ?>
+	</td>
 </tr>
