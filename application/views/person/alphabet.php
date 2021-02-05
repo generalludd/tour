8190c695babc4894175047f8cb23413382e9ff96
Chris Dart <chris@ten7.com>
2021-02-04 18:53:18 -0600

    Issue #80 fix alphabet.php display to only show single initial per row.

--- application/views/person/alphabet.php
+++ application/views/person/alphabet.php
@@ -1,14 +1,25 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 // alphabet.php Chris Dart Jan 1, 2014 8:05:15 PM chrisdart@cerebratorium.com
+$last_initial = NULL;
+$buttons = [];
+if(!empty($initials)) {
+	foreach ($initials as $initial) {
+		$active = "";
+		if ($initial->initial != $last_initial) {
+			if ($this->input->get("intial") == $initial->initial) {
+				$active = "active";
+			}
 
-foreach($initials as $initial){
-    $active = "";
-    if($this->input->get("intial") == $initial->initial){
-        $active = "active";
-    }
-$buttons[] = array("text"=>ucfirst($initial->initial),"href"=>site_url("person?initial=$initial->initial"),"class"=>"button letter $active");
+			$buttons[] = [
+				"text" => ucfirst($initial->initial),
+				"href" => site_url("person?initial=$initial->initial"),
+				"class" => "button letter $active"
+			];
+		}
+		$last_initial = $initial->initial;
+	}
+
+	print create_button_bar($buttons);
+
 }
-
-print create_button_bar($buttons);
-
