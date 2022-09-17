75b69858131c301f55e97b486a929ddc36552791
Chris Dart <chris@ten7.com>
2022-09-17 16:50:50 -0500

    Fixes for CURDATE() queries.

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -207,7 +207,7 @@ function format_money(float $int = NULL, string $format = "standard"): string {
 	if ($format == 'int') {
 		$int = round($int, 0);
 	}
-	$fmt = new NumberFormatter('en_US', NumberFormatter::CURRENCY);
+	$fmt = new \NumberFormatter('en_US', \NumberFormatter::CURRENCY);
 	return $fmt->formatCurrency($int, 'USD');
 }
 
