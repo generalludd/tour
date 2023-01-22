9a351fdf20bb9524a87d66bea0b38fe00dfd6ce8
Chris Dart <chris@ten7.com>
2023-01-22 08:57:56 -0600

    Bug fix with tours.

--- application/controllers/Tour.php
+++ application/controllers/Tour.php
@@ -52,8 +52,7 @@ class Tour extends MY_Controller {
 
 	function create() {
 		$data["action"] = "insert";
-		$data["tour"] = ['tourists'=> [];
-		$data['tourists'] = [];
+		$data["tour"] = ['tourists'=> []];
 		$this->load->view("tour/edit", $data);
 	}
 
