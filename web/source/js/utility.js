d28be4c2360d7c26f750a2dc89de5e3304ae5f9a
Chris Dart <571478+generalludd@users.noreply.github.com>
2024-02-03 12:16:01 -0600

    Issue #140 switch to npm

---
+++ web/source/js/utility.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+	// Locate any item with the class .message
+	const messages = document.querySelectorAll('.message');
+	messages.forEach(function (message) {
+		const closeBox = message.querySelector('button.close');
+		console.log('closeBox', closeBox);
+		if (closeBox) {
+			closeBox.addEventListener('click', function (event) {
+
+				message.classList.add('hidden');
+
+			});
+		}
+	});
+
+});
