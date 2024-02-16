88a226ce6dd0c28603ff518b86121311a6daaf77
Chris Dart <chris@ten7.com>
2024-02-15 20:05:01 -0600

    Minor fixes to settings.

--- web/application/config/config.php
+++ web/application/config/config.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
@@ -23,26 +23,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$allowed_domains = array($_SERVER['HTTP_HOST']);
-$default_domain  = 'db.ballparktours.net';
+$allowed_domains = [
+	'db.ballparktours.net',
+	'tours.cerebratorium.com',
+	'tours.ddev.site',
+];
+$default_domain = 'db.ballparktours.net';
 
-if (in_array($_SERVER['HTTP_HOST'], $allowed_domains, TRUE))
-{
-        $domain = $_SERVER['HTTP_HOST'];
-}
-else
-{
-        $domain = $default_domain;
-}
 
-if ( $_SERVER['SERVER_PORT'] == 443)
-{
-        $config['base_url'] = 'https://'.$domain;
-}
-else
-{
-        $config['base_url'] = 'http://'.$domain;
-}
+$config['base_url'] = 'https://' . $_SERVER['HTTP_HOST'];
+
+
 $site_name = 'Ballpark Tours';
 /*
 |--------------------------------------------------------------------------
@@ -71,7 +62,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'https';
+$config['uri_protocol'] = 'https';
 
 /*
 |--------------------------------------------------------------------------
@@ -95,7 +86,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language'] = 'english';
 
 /*
 |--------------------------------------------------------------------------
@@ -393,7 +384,7 @@ $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 7600;
 $config['sess_regenerate_destroy'] = FALSE;
-$config['sess_table_name']		= 'user_sessions';
+$config['sess_table_name'] = 'user_sessions';
 
 /*
 |--------------------------------------------------------------------------
@@ -410,11 +401,11 @@ $config['sess_table_name']		= 'user_sessions';
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
+$config['cookie_prefix'] = '';
+$config['cookie_domain'] = '';
+$config['cookie_path'] = '/';
+$config['cookie_secure'] = FALSE;
+$config['cookie_httponly'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
@@ -463,7 +454,7 @@ $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array();
+$config['csrf_exclude_uris'] = [];
 
 /*
 |--------------------------------------------------------------------------
