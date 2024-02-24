e6acc5d1c514ab83465515b4ffeed81d3f44aa09
Chris Dart <chris@ten7.com>
2024-02-23 20:25:00 -0600

    Fix errors in code on for elements.css and list.php

--- web/application/views/tourist/list.php
+++ web/application/views/tourist/list.php
@@ -1,7 +1,7 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-// list.php Chris Dart Dec 28, 2013 4:47:05 PM chrisdart@cerebratorium.com
+if (empty($tour)) {
+	return FALSE;
+}
 
 $this->load->helper("tour_helper");
 $total_due = 0;
@@ -38,136 +38,144 @@ $buttons['print'] = [
 ?>
 <h2><?php print $tour->tour_name; ?></h2>
 <div class="block">
-    <?php print create_button_bar($buttons); ?>
+	<?php print create_button_bar($buttons); ?>
 </div>
 
 <div class="block">
-    <table class="list">
-        <thead>
-            <tr>
-                <th class="no-wrap">Payer (&#42;) &amp; Tourists</th>
-                <th></th>
-                <th>Contact Info</th>
-                <th class="no-wrap">Payment Type<br />Price
-                </th>
-                <th>Paid</th>
-                <th>Discount</th>
-                <th class="no-wrap">Room Size<br />Rate
-                </th>
-                <th>Due</th>
-            </tr>
-        </thead>
-        <tbody>
+	<table class="list">
+		<thead>
+		<tr>
+			<th class="no-wrap">Payer (&#42;) &amp; Tourists</th>
+			<th></th>
+			<th>Contact Info</th>
+			<th class="no-wrap">Payment Type<br/>Price
+			</th>
+			<th>Paid</th>
+			<th>Discount</th>
+			<th class="no-wrap">Room Size<br/>Rate
+			</th>
+			<th>Due</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php if (!empty($payers)): ?>
+			<?php foreach ($payers as $payer) : ?>
+				<?php $total_payers++; ?>
+				<tr
+					class="row row-break<?php print $payer->is_cancelled == 1 ? " cancelled" : ""; ?>">
+					<td>
+						<?php foreach ($payer->tourists as $tourist) : ?>
+							<?php if ($payer->is_cancelled == 0) : ?>
+								<?php $total_tourists++; ?>
+								<?php update_shirt_count($shirt_count, $tourist->shirt_size); ?>
+							<?php endif; ?>
 
-            <?php foreach ($payers as $payer) : ?>
-            <?php $total_payers++; ?>
-            <tr class="row row-break<?php print $payer->is_cancelled == 1 ? " cancelled" : ""; ?>">
-                <td>
-                    <?php foreach ($payer->tourists as $tourist) : ?>
-                    <?php if ($payer->is_cancelled == 0) : ?>
-                    <?php $total_tourists++; ?>
-                    <?php update_shirt_count($shirt_count, $tourist->shirt_size); ?>
-                    <?php endif; ?>
-
-                    <?php $tourist_name = sprintf("%s %s", $tourist->first_name, $tourist->last_name); ?>
-                    <?php printf("<a href='%s' title='View %s&rsquo;s address book entry'>%s</a>", site_url("person/view/$tourist->person_id"), $tourist_name, $tourist_name); ?>
-                    <?php if ($tourist->person_id == $payer->payer_id) : ?>
-                    <?php print "*"; ?>
-                    <?php endif; ?>
-                    <?php if (get_value($tourist, "shirt_size", FALSE)) : ?>
-                    &nbsp;(<?php print $tourist->shirt_size; ?>)
-                    <?php endif; ?>
-                    <br />
-                    <?php endforeach; ?>
-                    <br />
-                    <?php if ($payer->merge_id) : ?>
-                    <a href="<?php print base_url('letter/select/' . $payer->payer_id . '/' . $payer->tour_id); ?>"
-                        class="button edit dialog" data-payer_id="<?php print $payer->payer_id; ?>"
-                        data-tour_id="<?php print $payer->tour_id; ?>">Edit
-                        Letter</a>
-                    <?php else : ?>
-                    <a <a href="<?php print base_url('letter/select/' . $payer->payer_id . '/' . $payer->tour_id); ?>"
-                        class="button new dialog" data-payer_id="<?php print $payer->payer_id; ?>"
-                        data-tour_id="<?php print $payer->tour_id; ?>">Send
-                        Letter</a>
-                    <?php endif; ?>
-                <td>
-                    <a href="<?php print site_url("payer/edit?payer_id=$payer->payer_id&tour_id=$payer->tour_id"); ?>"
-                        class="button edit">Edit
-                        Payment</a>
-                </td>
-                <td>
-                    <?php $this->load->view('person/contact_card',['person'=> $payer->person]); ?>
-                </td>
-                <?php
+							<?php $tourist_name = sprintf("%s %s", $tourist->first_name, $tourist->last_name); ?>
+							<?php printf("<a href='%s' title='View %s&rsquo;s address book entry'>%s</a>", site_url("person/view/$tourist->person_id"), $tourist_name, $tourist_name); ?>
+							<?php if ($tourist->person_id == $payer->payer_id) : ?>
+								<?php print "*"; ?>
+							<?php endif; ?>
+							<?php if (get_value($tourist, "shirt_size", FALSE)) : ?>
+								&nbsp;(<?php print $tourist->shirt_size; ?>)
+							<?php endif; ?>
+							<br/>
+						<?php endforeach; ?>
+						<br/>
+						<?php if ($payer->merge_id) : ?>
+							<a
+								href="<?php print base_url('letter/select/' . $payer->payer_id . '/' . $payer->tour_id); ?>"
+								class="button edit dialog"
+								data-payer_id="<?php print $payer->payer_id; ?>"
+								data-tour_id="<?php print $payer->tour_id; ?>">Edit
+								Letter</a>
+						<?php else : ?>
+							<a <a
+								href="<?php print base_url('letter/select/' . $payer->payer_id . '/' . $payer->tour_id); ?>"
+								class="button new dialog"
+								data-payer_id="<?php print $payer->payer_id; ?>"
+								data-tour_id="<?php print $payer->tour_id; ?>">Send
+								Letter</a>
+						<?php endif; ?>
+					<td>
+						<a
+							href="<?php print site_url("payer/edit?payer_id=$payer->payer_id&tour_id=$payer->tour_id"); ?>"
+							class="button edit">Edit
+							Payment</a>
+					</td>
+					<td>
+						<?php $this->load->view('person/contact_card', ['person' => $payer->person]); ?>
+					</td>
+					<?php
 					if ($payer->is_comp == 1) :
-					?>
-                <td>Complementary</td>
-                <?php elseif ($payer->is_cancelled == 1) : ?>
-                <td class='cancelled'>Cancelled</td>
-                <?php $total_cancels++; ?>
+						?>
+						<td>Complementary</td>
+					<?php elseif ($payer->is_cancelled == 1) : ?>
+						<td class='cancelled'>Cancelled</td>
+						<?php $total_cancels++; ?>
 
-                <?php else : ?>
-                <td><?php print sprintf("%s<br/>%s", format_field_name($payer->payment_type), format_money($payer->price)); ?>
-                </td>
-                <?php endif; ?>
-                <td><?php print format_money($payer->amt_paid); ?>
-                </td>
-                <td><?php print format_money($payer->discount); ?></td>
-                <td><?php print sprintf("%s<br/>%s", format_field_name($payer->room_size), format_money($payer->room_rate)); ?>
-                </td>
-                <td><?php echo $payer->is_cancelled == 1 ? 0 : format_money($payer->amt_due); ?>
-                </td>
-            </tr>
-            <?php if (get_value($payer, "note", FALSE)) : ?>
-            <tr>
-                <td></td>
-                <td colspan="10"><?php print get_value($payer, "note"); ?></td>
-            </tr>
-            <?php endif; ?>
-            <?php
+					<?php else : ?>
+						<td><?php print sprintf("%s<br/>%s", format_field_name($payer->payment_type), format_money($payer->price)); ?>
+						</td>
+					<?php endif; ?>
+					<td><?php print format_money($payer->amt_paid); ?>
+					</td>
+					<td><?php print format_money($payer->discount); ?></td>
+					<td><?php print sprintf("%s<br/>%s", format_field_name($payer->room_size), format_money($payer->room_rate)); ?>
+					</td>
+					<td><?php echo $payer->is_cancelled == 1 ? 0 : format_money($payer->amt_due); ?>
+					</td>
+				</tr>
+				<?php if (get_value($payer, "note", FALSE)) : ?>
+					<tr>
+						<td></td>
+						<td colspan="10"><?php print get_value($payer, "note"); ?></td>
+					</tr>
+				<?php endif; ?>
+				<?php
 				if ($payer->is_cancelled != 1) {
 					$total_due += $payer->amt_due;
 					$total_paid += $payer->amt_paid;
 				}
 				?>
 
-            <?php endforeach; ?>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td>
-                    Tourists: <?php print $total_tourists; ?>
-                </td>
-                <td>
-                    Payers: <?php print $total_payers - $total_cancels; ?>
-                </td>
-                <td>
-                    Cancels: <?php print $total_cancels; ?>
-                </td>
-                <td style="text-align: right;">
-                    Total Paid
-                </td>
-                <td colspan='2'>
-                    <?php print format_money($total_paid); ?>
-                </td>
-                <td style="text-align: right;">
-                    Total Due
-                </td>
-                <td>
-                    <?php print format_money($total_due); ?>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
-    <?php if ($shirt_count) : ?>
-    <p><strong>Shirt Totals</strong><br />
-        <?php print sort_shirts($shirt_count); ?>
-    </p>
-    <?php endif; ?>
+			<?php endforeach; ?>
+		<?php endif; ?>
 
-    <p>
-        <?php echo date("m-d-Y"); ?>
-    </p>
+		</tbody>
+		<tfoot>
+		<tr>
+			<td>
+				Tourists: <?php print $total_tourists; ?>
+			</td>
+			<td>
+				Payers: <?php print $total_payers - $total_cancels; ?>
+			</td>
+			<td>
+				Cancels: <?php print $total_cancels; ?>
+			</td>
+			<td style="text-align: right;">
+				Total Paid
+			</td>
+			<td colspan='2'>
+				<?php print format_money($total_paid); ?>
+			</td>
+			<td style="text-align: right;">
+				Total Due
+			</td>
+			<td>
+				<?php print format_money($total_due); ?>
+			</td>
+		</tr>
+		</tfoot>
+	</table>
+	<?php if ($shirt_count) : ?>
+		<p><strong>Shirt Totals</strong><br/>
+			<?php print sort_shirts($shirt_count); ?>
+		</p>
+	<?php endif; ?>
+
+	<p>
+		<?php echo date("m-d-Y"); ?>
+	</p>
 
 </div>
