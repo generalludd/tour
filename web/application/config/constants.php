9424124056fae8c46c2023cf0cc1fa731360acc8
Chris Dart <571478+generalludd@users.noreply.github.com>
2024-03-17 16:47:24 -0500

    Add release 3.8

--- web/application/config/constants.php
+++ web/application/config/constants.php
@@ -36,7 +36,7 @@ const FOPEN_READ_WRITE_CREATE = 'a+b';
 const FOPEN_WRITE_CREATE_STRICT = 'xb';
 const FOPEN_READ_WRITE_CREATE_STRICT = 'x+b';
 
-const APP_VERSION = "3.7";
+const APP_VERSION = "3.8";
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
 const BACKUP_THRESHOLD = 1209599;
