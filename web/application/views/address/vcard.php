029c6314174aecc5cf524542682b2ba7060fd5d5
Chris Dart <571478+generalludd@users.noreply.github.com>
2024-04-27 09:56:27 -0500

    Issue #153 Add notes values to vcard.

--- web/application/views/address/vcard.php
+++ web/application/views/address/vcard.php
@@ -1,21 +1,39 @@
 <?php
-if(isset($person)):?>
+if (isset($person)):
+	$notes = [];
+	if (!empty($person->note)):
+		$person_note = str_replace([";", ","], ["\;", "\,"], $person->note); // Escape commas and semicolons
+		$notes[] = str_replace("\n", "\\n", $person_note); // Replace newlines with \n
+	endif;
+	if (!empty($person->shirt_size)):
+		$notes[] = "Shirt size: " . $person->shirt_size;
+	endif;
+	if (!empty($person->is_veteran)):
+		$notes[] =  "BPT Veteran";
+	endif;
+		?>
 BEGIN:VCARD
 
 VERSION:3.0
 
-FN;CHARSET=UTF-8:<?php print $person->first_name . ' ' . $person->last_name;?>
+FN;CHARSET=UTF-8:<?php print $person->first_name . ' ' . $person->last_name; ?>
 
-N;CHARSET=UTF-8:<?php print $person->last_name;?>;<?php print $person->first_name; ?>;;;
+N;CHARSET=UTF-8:<?php print $person->last_name; ?>;<?php print $person->first_name; ?>;;;
 
 EMAIL;CHARSET=UTF-8;type=HOME,INTERNET:<?php print $person->email; ?>
 
-<?php foreach ($person->phones as $phone):?>
+<?php foreach ($person->phones as $phone): ?>
 TEL;TYPE=<?php print strtoupper($phone->phone_type); ?>:<?php print $phone->phone; ?>
 
 <?php endforeach; ?>
+
 ADR;CHARSET=UTF-8;TYPE=HOME:;;<?php print $person->address->address; ?>;<?php print $person->address->city; ?>;<?php print $person->address->state; ?>;<?php print $person->address->zip; ?>;USA
 
+<?php if (!empty($notes)): ?>
+<?php array_unshift($notes, "Notes from db.ballparktours.net:"); ?>
+NOTE:<?php print implode('\\n', $notes); ?>
+
+<?php endif; ?>
 END:VCARD
 <?php endif;
 
