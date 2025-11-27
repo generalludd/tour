591d082d46c71c961cb55f26a61737ef6c09049e
Chris Dart <chris@cerebratorium.com>
2025-11-26 21:16:05 -0600

    Add local email functinality

--- web/application/config/email.php
+++ web/application/config/email.php
@@ -1,16 +1,3 @@
 <?php
 
-switch($_SERVER['HTTP_HOST']){
-	case 'docker.test':
-	case 'backoffice.test':
-	$config['protocol'] = 'smtp';
-	$config['smtp_host'] = 'mailhog';
-	$config['smtp_port'] = '1024';
-	$config['smtp_crypto'] = '';
-		$config['smtp_user'] = '';
-		$config['smtp_pass'] = '';
-		break;
-	default:
-
-		break;
-}
+include 'email.local.php';
